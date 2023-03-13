9fec0d33c726f0f76ddb15f825797e63cd2164a6
Johannes Kasser <johannes.kasser@stud.fh-campuswien.ac.at>
2023-03-13 11:46:11 +0100

    Setup for Local and Prodiuction environment

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+require('dotenv').config()
 
-axios.defaults.baseURL = 'https://p10racing.herokuapp.com/api';
+axios.defaults.baseURL = process.env.VUE_APP_BaseURLBackend;
 axios.defaults.withCredentials = true;
 
 export default axios;
